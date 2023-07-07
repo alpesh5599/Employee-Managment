7db0d39f221a6445a34e3dc0fca91feb6e3d1fe8
Alpesh Makwana <05051999am@gmail.com>
2023-07-07 12:29:12 +0530

    imports in main files removed

--- emp_management/emp_management/src/main/java/com/nexscend/employee/management/EmpManagementApplication.java
+++ emp_management/emp_management/src/main/java/com/nexscend/employee/management/EmpManagementApplication.java
@@ -4,10 +4,8 @@ import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.nexscend.employee.management.property.DocumentStorageProperty;
 import com.nexscend.employee.management.utils.Constants;
 
 @SpringBootApplication(scanBasePackages = "com.nexscend.employee.management")
